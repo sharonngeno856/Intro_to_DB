7c6279cd05d7af474af2a631d0a1bc52f9bb4578
Sharon Chepkemoi <sharonngeno23@gmail.com>
2025-08-17 19:44:01 +0300

    Add schema and GitHub Action for MySQL testing

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,46 +1,46 @@
--- Create Database
-CREATE DATABASE alx_book_store;
+-- CREATE DATABASE
+CREATE DATABASE IF NOT EXISTS alx_book_store;
 
--- Use Database
+-- USE DATABASE
 USE alx_book_store;
 
--- Authors Table
-CREATE TABLE Authors (
+-- AUTHORS TABLE
+CREATE TABLE IF NOT EXISTS Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
 
--- Books Table
-CREATE TABLE Books (
+-- BOOKS TABLE
+CREATE TABLE IF NOT EXISTS Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
-    author_id INT,
+    author_id INT NOT NULL,
     price DOUBLE NOT NULL,
     publication_date DATE,
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
--- Customers Table
-CREATE TABLE Customers (
+-- CUSTOMERS TABLE
+CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) UNIQUE NOT NULL,
     address TEXT
 );
 
--- Orders Table
-CREATE TABLE Orders (
+-- ORDERS TABLE
+CREATE TABLE IF NOT EXISTS Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_id INT,
+    customer_id INT NOT NULL,
     order_date DATE,
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
--- Order_Details Table
-CREATE TABLE Order_Details (
+-- ORDER_DETAILS TABLE
+CREATE TABLE IF NOT EXISTS Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
-    order_id INT,
-    book_id INT,
+    order_id INT NOT NULL,
+    book_id INT NOT NULL,
     quantity DOUBLE NOT NULL,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
